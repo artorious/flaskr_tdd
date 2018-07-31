43e019bcdafc66a6568f6cd0808a15b0f3066913
Arthur Ngondo <ngondoarthur@gmail.com>
2018-07-31 18:21:33 +0300

    Add flaskr set-up file
    Add and test configuration file

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+""" Configuration and App Factory """
+import sqlite3
+from flask import (
+    Flask, request, session, g, redirect, url_for, abort, \
+    abort, render_template, flash, jsonify
+)
+
+
+DATABASE = 'flaskr.db'
+DEBUG = True
+SECRET_KEY = 'my_precious'
+USERNAME = 'admin'
+PASSWORD = 'admin'
 
 app = Flask(__name__)
+app.config.from_object(__name__)
 
-
-@app.route('/')
-def hello():
-    return 'Hello, World!'
 
 if '__name__' == '__main__':
     app.run()
+
