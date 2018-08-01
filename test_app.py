8c593d811f65ea33019829e2dd8f10d0431fb945
Arthur Ngondo <ngondoarthur@gmail.com>
2018-08-01 09:56:28 +0300

    Setup database
    * Open a database connection.
    * Create the database on schema.sql if it doesn't exist.

--- test_app.py
+++ test_app.py
@@ -1,14 +1,22 @@
-from app import app
+""" Tests for Application """
 import unittest
+import os
+from app import app
 
 
 class BasicTestCase(unittest.TestCase):
 
     def test_index(self):
-        """Test index route.""" 
+        """Test flask setup - Initial test""" 
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 404)
+    
+    def test_database(self):
+        """Test database exists - Initial test"""
+        tester = os.path.exists("flaskr.db")
+        self.assertTrue(tester)
+
 
 if __name__ == '__main__':
     unittest.main()
